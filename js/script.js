3bdda87c74bc005ca09e55f1921c7615024deda5
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-10-11 12:11:39 -0500

    Creates function and properties

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+// creates function and properties
+const createPet = function (name, species) {
+    const pet = {
+      name : name,
+      species: species,
+      isTired: 5,
+      sleep: function () {
+        console.log(`${name} needs a nap. Zzzz...`);
+        this.isTired = 1;
+        
+      }
+    }
+}
